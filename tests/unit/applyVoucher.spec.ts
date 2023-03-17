2378c0e522734439ff5b35f30e09f32502a8fe3f
kcandeloni <kcandeloni@gmail.com>
2023-03-16 23:15:39 -0300

    feat: Unit teststing to create voucher and apply voucher

--- tests/unit/applyVoucher.spec.ts
+++ tests/unit/applyVoucher.spec.ts
@@ -2,8 +2,42 @@ import { jest } from "@jest/globals";
 import voucherService from "services/voucherService";
 import voucherRepository from "repositories/voucherRepository";
 
+const params = {code:"CODE10", amount: 100, valueRejected: 90, discont: 10};
+
 describe("Apply Voucher", () => {
-  it("Apply nonexistent voucer", async () => {
+  it("Voucher applied successfully", async () => {
+    jest
+    .spyOn(voucherRepository, "getVoucherByCode")
+    .mockImplementationOnce((code): any => {
+      return {
+        id: 1,
+        code: code,
+        discount: params.discont,
+        used: false
+      };
+    });
+
+    jest
+    .spyOn(voucherRepository, "useVoucher")
+    .mockImplementationOnce((): any => {
+      return;
+    });
+
+    const promise = await voucherService.applyVoucher(params.code, params.amount);
+    
+    expect(promise).toEqual({
+      amount: params.amount,
+      discount: params.discont,
+      finalAmount: params.amount - (params.amount * (params.discont / 100)),
+      applied: true
+    });
+  });
+  
+});
+
+describe("Reject voucher", () => {
+  
+  it("Non-existent voucher", async () => {
     jest
     .spyOn(voucherRepository, "getVoucherByCode")
     .mockImplementationOnce((): any => {
@@ -13,5 +47,61 @@ describe("Apply Voucher", () => {
     const promise = voucherService.applyVoucher("CODE10", 100);
     expect(promise).rejects.toEqual({message: "Voucher does not exist.", type: "conflict"});
   });
+  
+  it("Minimum amount for discount not reached", async () => {
+    jest
+    .spyOn(voucherRepository, "getVoucherByCode")
+    .mockImplementationOnce((code): any => {
+      return {
+        id: 1,
+        code: code,
+        discount: params.discont,
+        used: false
+      };
+    });
 
+    jest
+    .spyOn(voucherRepository, "useVoucher")
+    .mockImplementationOnce((): any => {
+      return;
+    });
+
+    const promise = await voucherService.applyVoucher(params.code, params.valueRejected);
+    
+    expect(promise).toEqual({
+      amount: params.valueRejected,
+      discount: params.discont,
+      finalAmount: params.valueRejected,
+      applied: false
+    });
+  });
+  
+  it("Voucher already used", async () => {
+    jest
+    .spyOn(voucherRepository, "getVoucherByCode")
+    .mockImplementationOnce((code): any => {
+      return {
+        id: 1,
+        code: code,
+        discount: params.discont,
+        used: true
+      };
+    });
+
+    jest
+    .spyOn(voucherRepository, "useVoucher")
+    .mockImplementationOnce((): any => {
+      return;
+    });
+
+    const promise = await voucherService.applyVoucher(params.code, params.amount);
+    
+    expect(promise).toEqual({
+      amount: params.amount,
+      discount: params.discont,
+      finalAmount: params.amount,
+      applied: false
+    });
+  });
+  
 });
--- tests/unit/createVoucher.spec.ts
+++ tests/unit/createVoucher.spec.ts
@@ -2,8 +2,8 @@ import { jest } from "@jest/globals";
 import voucherService from "services/voucherService";
 import voucherRepository from "repositories/voucherRepository";
 
-describe("Create Voucher", () => {
-  it("Create new voucer", async () => {
+describe("Reject Voucher Creation", () => {
+  it("Try to create an existing voucher", async () => {
     jest
     .spyOn(voucherRepository, "getVoucherByCode")
     .mockImplementationOnce((): any => {
@@ -12,6 +12,27 @@ describe("Create Voucher", () => {
 
     const promise = voucherService.createVoucher("newvoucher", 10);
     expect(promise).rejects.toEqual({message: "Voucher already exist.", type: "conflict"});
+  });
+
+});
+
+describe("Create Voucher", () => {
+  it("Create new voucer", () => {
+    expect( async () => {
+        jest
+      .spyOn(voucherRepository, "getVoucherByCode")
+      .mockImplementationOnce((): any => {
+        return false;
+      });
+
+      jest
+      .spyOn(voucherRepository, "createVoucher")
+      .mockImplementationOnce((): any => {
+        return;
+      });
+
+      await voucherService.createVoucher("newvoucher", 10);
+    }).not.toEqual({message: "Voucher already exist.", type: "conflict"});
   });
 
 });
